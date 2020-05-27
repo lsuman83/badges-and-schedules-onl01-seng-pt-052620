da4f473116a1af71863b1fcc4e72ef5990e30706
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-05-27 03:56:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,28 @@
 # Write your code here.
 def badge_maker (name)
+  
   return "Hello, my name is #{name}."
+  
 end
 
 def batch_badge_creator (badges)
   
   index = 0
+  
   badges.each do |names|
      badges[index] = "Hello, my name is #{names}."
      index += 1
   end
+  
+end
+
+def assign_rooms (rooms)
+  
+  index = 0
+  
+  rooms.each do |name|
+    rooms [index] = "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    index += 1
+  end
+
 end
