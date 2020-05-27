7a79604ea69245bc15dce53c708d97e7e366d854
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-05-27 04:30:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -27,15 +27,13 @@ def assign_rooms (rooms)
 
 end
 
-require "pry"
-
 def printer (attendees)
   
   batch_badge_creator(attendees).each do |badge|
     puts "#{badge}"
   end
   
-  assign_rooms.each do |room|
+  assign_rooms(attendees).each do |room|
     puts "#{room}"
   end
   
