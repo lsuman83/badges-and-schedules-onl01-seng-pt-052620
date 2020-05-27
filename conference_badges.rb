889d813471898a9cd8fcb8f0d7ab205035b07b28
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-05-27 03:43:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker (name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_maker (names)
+def batch_badge_creator (names)
   names.each do |badge|
     puts "Hello, my name is #{badge}."
   end
